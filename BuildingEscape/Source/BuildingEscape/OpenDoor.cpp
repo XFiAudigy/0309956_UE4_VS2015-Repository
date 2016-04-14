5300aab8aa17043d4d420dd3fd976250a8849eba
xfiaudigy <jxdot567@gmail.com>
2016-04-14 17:06:11 +0800

    Rotations

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,22 +17,20 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	FString OwnerName = GetOwner()->GetName();
-	FVector OwnerVector = GetOwner()->GetActorLocation();
-	FString OwnerVectStr = FString::Printf(TEXT("X = %f, Y = %f, Z = %f"), OwnerVector.X, OwnerVector.Y, OwnerVector.Z);
-
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *OwnerName, *OwnerVectStr);
-
+	AActor* Owner = GetOwner();
+	
+	FRotator newRotation = FRotator(0.0f, -60.0f, 0.0f);
+	Owner->SetActorRotation(newRotation);
 	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
--- BuildingEscape/Source/BuildingEscape/PositionReport2.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport2.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "PositionReport2.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UPositionReport2::UPositionReport2()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -17,22 +17,22 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UPositionReport2::BeginPlay()
 {
 	Super::BeginPlay();
+
 	FString OwnerName = GetOwner()->GetName();
 	FVector OwnerVector = GetOwner()->GetActorLocation();
 	FString OwnerVectStr = FString::Printf(TEXT("X = %f, Y = %f, Z = %f"), OwnerVector.X, OwnerVector.Y, OwnerVector.Z);
 
 	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *OwnerName, *OwnerVectStr);
-
 	// ...
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UPositionReport2::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
